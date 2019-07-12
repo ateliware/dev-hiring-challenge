ebc8a2fb93e66653aa51ebbd3081a7f1db224ab9
boaglio <boaglio@gmail.com>
2019-07-12 08:50:56 -0300

    Github API test

---
+++ src/main/java/br/com/ateliware/challenge/AteliwareChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ateliware.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteliwareChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteliwareChallengeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/ateliware/challenge/Language.java
@@ -0,0 +1,7 @@
+package br.com.ateliware.challenge;
+
+public enum Language {
+
+    java, javascript, python, ruby
+    
+}
