b0633cb75dbcbf4e8ff4d6ad32316ef826f18e6e
Mario Alvial <mse.alvial@gmail.com>
2019-07-20 19:49:20 -0300

    Add dynamic status logic for health endpoint

--- src/main/kotlin/dev/hiring/challenge/application/health/HealthHandler.kt
+++ src/main/kotlin/dev/hiring/challenge/application/health/HealthHandler.kt
@@ -1,15 +1,22 @@
 package dev.hiring.challenge.application.health
 
 import io.github.marioalvial.kealth.core.HealthAggregator
+import io.github.marioalvial.kealth.core.HealthStatus
+import io.github.marioalvial.kealth.core.HealthStatus.*
 import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.http.HttpStatusCode.Companion.ServiceUnavailable
 import io.ktor.response.respond
 
 class HealthHandler(
         private val healthAggregator: HealthAggregator
 ) {
 
-    suspend fun health(call: ApplicationCall){
-        call.respond(HttpStatusCode.OK, healthAggregator.aggregate())
+    suspend fun health(call: ApplicationCall) {
+        val aggregate = healthAggregator.aggregate()
+        val status = if (aggregate.any { it.value.status == UNHEALTHY }) ServiceUnavailable else OK
+
+        call.respond(status, aggregate)
     }
 }
