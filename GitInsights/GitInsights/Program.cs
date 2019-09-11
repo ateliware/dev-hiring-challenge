34d1e00531ad9c83bd8d578b17dde5d46c1d114b
aureliomatsunaga <aureliomatsunaga@gmail.com>
2019-09-11 07:58:06 -0300

    Deployment successfull

--- GitInsights/GitInsights/Program.cs
+++ GitInsights/GitInsights/Program.cs
@@ -1,24 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace GitInsights
 {
-	public class Program
+    public class Program
 	{
 		public static void Main(string[] args)
 		{
 			CreateWebHostBuilder(args).Build().Run();
 		}
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		{
+			var port = Environment.GetEnvironmentVariable("PORT");
+
+			return WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseUrls("http://*:"+port);
+		}
 	}
 }
