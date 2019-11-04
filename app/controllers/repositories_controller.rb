2565e51b37525d2395ad1a5fc6893eecc5059279
Rodrigo Fernandes (rofrnnds) <rodrigodeoliveira.ti@gmail.com>
2019-11-04 20:57:21 -0300

    Repositories controller boilerplate

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,9 @@
 class RepositoriesController < ApplicationController
   def index
+    #TODO: Implement filtering by language
+    @repos = Repo.all
   end
   def show
+    @repo = Repo.find(params[:id])
   end
 end
