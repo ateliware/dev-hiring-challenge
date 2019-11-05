139f10d922663a6dead0b8d1811141a2ea4c7cf9
Rodrigo Fernandes (rofrnnds) <rodrigodeoliveira.ti@gmail.com>
2019-11-05 10:05:59 -0300

    Removed the welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get 'clear_data'
     end
   end
-  root 'welcome#index'
+  get '/', to: redirect('/repositories')
+  root 'repositories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
