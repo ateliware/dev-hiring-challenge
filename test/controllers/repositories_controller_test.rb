cd0e04a0083116088ba3cdf7a6c8bf350f40e269
Rodrigo Fernandes (rofrnnds) <rodrigodeoliveira.ti@gmail.com>
2019-11-05 09:32:36 -0300

    Added controller tests

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -4,4 +4,22 @@ class RepositoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do 
+    @repo = repos(:one)
+  end
+
+  test 'should get index' do
+    get repositories_url
+    assert_response :success
+  end
+
+  test 'should fetch repositories data' do
+    get fetch_data_repositories_url
+    assert_redirected_to root_url
+  end
+
+  test 'should clear repositories data' do
+    get clear_data_repositories_url
+    assert_redirected_to root_url
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,4 +4,9 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should get root' do
+    get root_url
+    assert_response :success
+  end
 end
