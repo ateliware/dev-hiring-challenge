800ce73111a252ee161df36e092a75ab5cc2583b
George Bentes <george.bentes@gmail.com>
2020-04-28 16:22:03 -0300

    Add externalization of frontend CORS url

--- github-api/src/main/java/com/grglucastr/githubapi/config/WebConfig.java
+++ github-api/src/main/java/com/grglucastr/githubapi/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.grglucastr.githubapi.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${application.frontend.host}")
+    private String frontendHost;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontendHost)
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS");
 
     }
