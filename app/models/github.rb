64b382a05e4d8d54dc10e3654b6ada65b646aee7
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-05-25 10:11:54 -0300

    Refactor tests for github model

--- app/models/github.rb
+++ app/models/github.rb
@@ -2,19 +2,15 @@ class Github
   include HTTParty
   base_uri 'api.github.com'
 
-  def initialize
-    @auth = { headers: { "Authorization" => "token #{ENV['GITHUB_TOKEN']}" } }
-  end
-
-  def user
-    res = self.class.get('/user', @auth)
-  end
-
-  def search_repositories(query_string)
-    res = self.class.get("/search/repositories?q=#{query_string}", @auth)
+  def initialize(github_token = ENV['GITHUB_TOKEN'])
+    @auth = { headers: { 'Authorization' => "token #{github_token}" } }
   end
 
   def repositories(id)
-    res = self.class.get("/repositories/#{id}", @auth)
+    self.class.get("/repositories/#{id}", @auth)
+  end
+
+  def search_repositories(query_string)
+    self.class.get("/search/repositories?q=#{query_string}", @auth)
   end
 end
--- spec/models/github_spec.rb
+++ spec/models/github_spec.rb
@@ -1,19 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Github do
-  describe '#user' do
-    it 'returns status 200' do
-      github = Github.new
-      response = github.user
-      expect(response.code).to be(200)
+  describe '#search_repositories' do
+    context 'when given an authorization token' do
+      let(:token) { 'token' }
+
+      context 'when given a query string' do
+        let(:query_string) { 'language:ruby' }
+
+        it "calls the HTTParty's get method with correct arguments" do
+          github = Github.new(token)
+          expect(Github).to receive(:get).with(
+            "/search/repositories?q=#{query_string}",
+            { headers: { 'Authorization' => "token #{token}" } }
+          )
+          github.search_repositories(query_string)
+        end
+      end
     end
   end
 
   describe '#repositories' do
-    it 'returns status 200' do
-      github = Github.new
-      response = github.search_repositories('language:ruby')
-      expect(response.code).to be(200)
+    context 'when given an authorization token' do
+      let(:token) { 'token' }
+
+      context "when given a repository id" do
+        let(:id) { 1 }
+
+        it "calls the HTTPary's get method with correct arguments" do
+          github = Github.new(token)
+          expect(Github).to receive(:get).with(
+            "/repositories/#{id}",
+            { headers: { 'Authorization' => "token #{token}" } }
+          )
+          github.repositories(id)
+        end
+      end
     end
   end
 end
