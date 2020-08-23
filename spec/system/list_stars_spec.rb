cfec287cbfd9dd8eaa28b91facb62af9af8ee190
Matheus Santana <matheus@guava.software>
2020-08-23 15:59:42 -0300

    Style with Bootstrap

--- spec/system/list_stars_spec.rb
+++ spec/system/list_stars_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'List stars', type: :system do
     Star.create(name: 'rails/rails')
 
     visit '/'
-    click_on 'Find stars'
+    click_on 'Find GitHub stars'
 
     expect(page).to have_content 'rails/rails'
   end
