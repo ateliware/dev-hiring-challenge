8d03c9687eb429ee3f780aecec8b1d20952f979c
Aron Richter <aron.richter@contabilizei.com.br>
2020-09-19 18:39:55 -0300

    alterado configurações do backend para facilitar o deploy

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 const repositorioService = require("./service/repositorio.service.js")
 
