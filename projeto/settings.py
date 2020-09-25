0dd38da9ab2603cb0a2496a0e3faab0e6a2d4867
Lucas Villela <23728459+lcsvillela@users.noreply.github.com>
2020-09-25 15:24:50 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -133,5 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
