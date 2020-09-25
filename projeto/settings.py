b4e32f4c8fb9a03c4cfe0f7a358a42d2452fcdcc
Lucas Villela <23728459+lcsvillela@users.noreply.github.com>
2020-09-25 08:12:56 -0300

    Update settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'projeto.urls'
