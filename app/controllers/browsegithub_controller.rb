e29584149701d5772ab9f65e0df09178b004efd0
Ricardo Barros <richardtrle@gmail.com>
2020-11-30 19:24:12 -0300

    Changing route

--- app/controllers/browsegithub_controller.rb
+++ app/controllers/browsegithub_controller.rb
@@ -1,12 +1,7 @@
 class BrowsegithubController < ApplicationController
 
   def index
-    repos = Repository.all
-    repos = repos.map do |repo|
-      {id: repo.id, name: repo.name, description: repo.description, 
-      language: repo.language, stars: repo.stars}
-    render json: {results: repos}.to_json, status: :ok
-    end
+
   end
   
   def repositories
