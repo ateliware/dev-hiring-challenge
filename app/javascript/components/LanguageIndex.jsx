92491fe9e41475343d5fd0e01ccfc2c1199c06fc
Bruno Freitas Tissei <bft15@inf.ufpr.br>
2021-03-11 16:09:02 -0300

    Add "update repositories" button

--- app/javascript/components/LanguageIndex.jsx
+++ app/javascript/components/LanguageIndex.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import LanguageShow from "./LanguageShow";
+import { Button } from "react-bootstrap";
+
+import consumer        from "../channels/consumer";
+import LanguageShow    from "./LanguageShow";
 import RepositoryModal from "./RepositoryModal";
 
 /**
@@ -8,12 +11,12 @@ import RepositoryModal from "./RepositoryModal";
  * 
  * Props: { languages }
  * 
- * State: { showModal, repoInfo }
+ * State: { showModal, repoInfo, languageId: { movingButton, repositories } }
  * 
  * Children: [ LanguageShow, RepositoryModal ]
  * - LanguageShow.props:
- *  - attributes: { language }
- *  - callbacks: { openModal }
+ *  - attributes: { language, languageState }
+ *  - callbacks: { openModal, sendUpdateRequest }
  * - RepositoryModal.props:
  *  - attributes: { show, repository }
  *  - callbacks: { handleClose }
@@ -25,36 +28,95 @@ class LanguageIndex extends React.Component {
   constructor(props) {
     super(props);
 
+    var stage = this.props.languages.map((x) => x.id).reduce((map, obj) => {
+      map[obj] = {
+          movingButton: false,
+          repositories: []
+      };
+      return map;
+    }, {});
+
     this.state = {
       showModal: false,
-      repoInfo: null
+      repoInfo: null,
+      ...stage
     };
+
+    
+    this.subscription = null;
 
     this.setModal = this.setModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.sendUpdateRequest = this.sendUpdateRequest.bind(this);
+    this.sendUpdateAllRequests = this.sendUpdateAllRequests.bind(this);
   }
 
-  render () {
+  render() {
     return (
-      <>
-        <ul>
-          {
-            this.props.languages.map((language) => 
-              <li className="name" key={language.id}>
-                <LanguageShow
-                  language={language}
-                  openModal={this.setModal} />
-              </li>
-            )
-          }
-        </ul>
+      <div className="content">
+        <Button
+          onClick={this.sendUpdateAllRequests}
+          className="update-button">
+          Update repositories
+        </Button>
+
+        <div className="list">
+          <ul>
+            {
+              this.props.languages.map((language) => 
+                <li className="name" key={language.id}>
+                  <LanguageShow
+                    language={language}
+                    openModal={this.setModal}
+                    languageState={this.state[language.id]}
+                    sendUpdateRequest={this.sendUpdateRequest} />
+                </li>
+              )
+            }
+          </ul>
+        </div>
 
         <RepositoryModal
           show={this.state.showModal}
           repository={this.state.repoInfo}
           handleClose={this.closeModal} />
-      </>
+      </div>
     );
+  }
+
+  sendUpdateAllRequests() {
+    this.props.languages.forEach((x) => this.sendUpdateRequest(x.id))
+  }
+
+  // Request for repositories update for languageId
+  sendUpdateRequest(languageId) {
+    const csrf = document
+      .querySelector("meta[name='csrf-token']")
+      .getAttribute("content");
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrf
+      }
+    };
+
+    const url = `languages/${languageId}/update_repositories`;
+
+    this.setState({
+      [languageId]: {
+        movingButton: true,
+        repositories: []
+      }
+    });
+
+    fetch(url, requestOptions)
+      .then(data => {
+        if (data.status != 204) {
+          console.log("Error");
+        }
+      });
   }
 
   // Request repoId's attributes and set state, revealing modal
@@ -87,6 +149,44 @@ class LanguageIndex extends React.Component {
     this.setState({
       showModal: false,
       repoInfo: null
+    });
+  }
+
+  // Calls WebSocket subscription when component mounts
+  componentDidMount() {
+    this.setupSubscription();
+  }
+
+  componentWillUnmount() {
+    if (this.subscription) {
+      ThemeConsumer.subscription.remove(this.subscription);
+    }
+  }
+
+  // Callback for when data is received through subscription
+  updateRepositoryList(data) {
+    this.setState({
+      [data.language.id]: { 
+        repositories: data.language.repositories,
+        movingButton: false
+      }
+    });
+  }
+
+  // The subscription will signal "received", when request for repositories 
+  // update is finished and the back-end sends the data through WebSockets,
+  // which will trigger updateRepositoryList method
+  setupSubscription() {
+    this.subscription = consumer.subscriptions.create("RepositoriesChannel", {
+      connected() {},
+
+      disconnected() {},
+
+      received(data) {
+        this.update(data);
+      },
+
+      update: this.updateRepositoryList.bind(this)
     });
   }
 }
--- app/javascript/components/LanguageShow.jsx
+++ app/javascript/components/LanguageShow.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import consumer from "../channels/consumer";
 
 import { Card } from "react-bootstrap";
-import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
 
 import RepositoryShow from "./RepositoryShow";
 
@@ -34,7 +32,7 @@ class UpdateButton extends React.Component {
 /**
  * LanguageShow component
  * 
- * Props: { language, openModal }
+ * Props: { language, openModal, languageState, sendUpdateRequest }
  * 
  * State: { movingButton, repositories }
  * 
@@ -53,30 +51,25 @@ class LanguageShow extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      movingButton: false,
-      repositories: []
-    };
-
-    this.subscription = null;
     this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   render () {
     const language = this.props.language;
+    
     return (
       <Card bg="dark" style={{ width: '270px' }} text="white" className="mb-2">
         <Card.Header>
           {language.name}
 
           <UpdateButton
-            moving={this.state.movingButton}
+            moving={this.props.languageState.movingButton}
             handleClick={this.handleUpdate} />
         </Card.Header>
 
         <Card.Body style={{ padding: "0px" }}>
           { 
-            this.state.repositories.map((repository) =>
+            this.props.languageState.repositories.map((repository) =>
               <RepositoryShow
                 repository={repository}
                 key={repository.id}
@@ -90,69 +83,8 @@ class LanguageShow extends React.Component {
 
   // Request for repositories update for this language
   handleUpdate() {
-    const csrf = document
-      .querySelector("meta[name='csrf-token']")
-      .getAttribute("content");
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrf
-      }
-    };
-
-    const url = `languages/${this.props.language.id}/update_repositories`;
-
-    fetch(url, requestOptions)
-      .then(data => {
-        if (data.status == 204) {
-          this.setState({
-            movingButton: true
-          });
-        } else {
-          console.log("Error");
-        }
-      });
-  }
-
-  componentDidMount() {
-    this.setupSubscription();
-  }
-
-  componentWillUnmount() {
-    if (this.subscription) {
-      ThemeConsumer.subscription.remove(this.subscription);
-    }
-  }
-
-  updateRepositoryList(data) {
-    this.setState({
-      movingButton: false,
-      repositories: data.repositories
-    });
-  }
-
-  // The subscription will signal "received", when request for repositories 
-  // update is finished and the back-end sends the data through WebSockets,
-  // which will trigger updateRepositoryList method
-  setupSubscription() {
-    this.subscription = consumer.subscriptions.create("RepositoriesChannel", {
-      languageId: this.props.language.id,
-
-      connected() {},
-
-      disconnected() {},
-
-      received(data) {
-        if (data.language.id == this.languageId) {
-          this.update(data.language);
-        }
-      },
-
-      update: this.updateRepositoryList.bind(this)
-    });
-  }
+    this.props.sendUpdateRequest(this.props.language.id);
+  }  
 }
 
 export default LanguageShow
