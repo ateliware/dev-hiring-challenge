49054a87e8f1264131b68ba754a2ed3655aee2a1
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-13 21:13:44 -0300

    Setup annotate

--- github-details/app/models/organization.rb
+++ github-details/app/models/organization.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id          :bigint           not null, primary key
+#  avatar_url  :string
+#  description :string
+#  name        :string
+#  slug        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Organization < ApplicationRecord
-
   after_create :fetch_attributes
 
   private
--- github-details/test/models/organization_test.rb
+++ github-details/test/models/organization_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id          :bigint           not null, primary key
+#  avatar_url  :string
+#  description :string
+#  name        :string
+#  slug        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
