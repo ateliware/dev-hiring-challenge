b609718bdec0597b31ada83e821142f8341df4a7
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-13 22:47:11 -0300

    Add root route

--- github-details/config/routes.rb
+++ github-details/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "organizations#index"
+
   resources :repositories
   resources :organizations, param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
