c70e071925bf4d9fdb12fa74280e8ac3ded62f21
Bruno Freitas Tissei <bft15@inf.ufpr.br>
2021-03-14 14:18:08 -0300

    Add production settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
