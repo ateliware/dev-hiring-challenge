f72355ed659f7981822a1ceef7b08e80edc94bb7
Bianka Passos <biankatpas@gmail.com>
2021-03-16 00:27:54 -0300

    request.get

--- app/views.py
+++ app/views.py
@@ -9,29 +9,24 @@ from .models import Repo
 
 def home(request):
     Repo.objects.all().delete()
-    # todo
-    language = ['java', 'ruby', 'python', 'c++', 'kotlin']
     all_repos = {}
-    # for l in language:
-    # url =
-    # 'https://api.github.com/search/repositories?
-    # q=%s
-    # ' % l'
-    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
-    response = requests.get(url)
-    data = response.json()
-    repos = data['items']
-    for i in repos:
-        repo_data = Repo(
-            name=i['name'],
-            language=i['language'],
-            stars=i['stargazers_count'],
-            forks=i['forks_count'],
-            description=i['description'],
-            url=i['html_url'],
-        )
-        repo_data.save()
-        all_repos = Repo.objects.all().order_by('-stars')
+    if request.GET:
+        language = request.GET.get('nome')
+        url = 'https://api.github.com/search/repositories?q=language:'+language+'&sort=stars&order=desc'
+        response = requests.get(url)
+        data = response.json()
+        repos = data['items']
+        for i in repos:
+            repo_data = Repo(
+                name=i['name'],
+                language=i['language'],
+                stars=i['stargazers_count'],
+                forks=i['forks_count'],
+                description=i['description'],
+                url=i['html_url'],
+            )
+            repo_data.save()
+            all_repos = Repo.objects.all().order_by('-stars')
 
     return render(request, 'home.html', {
         "repos": all_repos,
