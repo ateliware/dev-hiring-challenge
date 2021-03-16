e1fce31383515398c1d2af6498fb536d7e44cb4f
Bianka Passos <biankatpas@gmail.com>
2021-03-16 00:30:21 -0300

    model

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ class Repo(models.Model):
     name = models.CharField(max_length=100)
     language = models.CharField(max_length=30)
     description = models.TextField(null=True)
-    watchers = models.IntegerField(null=True)
     stars = models.IntegerField(null=True)
     forks = models.IntegerField(null=True)
     url = models.CharField(max_length=140)
