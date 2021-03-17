ac228460f91138b94cc3d1d585f0473963e1ef93
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-16 23:19:00 -0300

    Fix requests rspecs

--- github-details/spec/requests/repositories_spec.rb
+++ github-details/spec/requests/repositories_spec.rb
@@ -13,44 +13,29 @@
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/repositories", type: :request do
-  
-  # Repository. As you add validations to Repository, be sure to
-  # adjust the attributes here as well.
+
+  let(:organization) {
+    Organization.create(slug: "python")
+  }
+
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {
+      name: "peps",
+      organization_id: organization.id,
+    }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {
+      name: nil,
+      organization_id: 999,
+    }
   }
 
   describe "GET /index" do
     it "renders a successful response" do
       Repository.create! valid_attributes
       get repositories_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      repository = Repository.create! valid_attributes
-      get repository_url(repository)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_repository_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "render a successful response" do
-      repository = Repository.create! valid_attributes
-      get edit_repository_url(repository)
       expect(response).to be_successful
     end
   end
@@ -62,11 +47,6 @@ RSpec.describe "/repositories", type: :request do
           post repositories_url, params: { repository: valid_attributes }
         }.to change(Repository, :count).by(1)
       end
-
-      it "redirects to the created repository" do
-        post repositories_url, params: { repository: valid_attributes }
-        expect(response).to redirect_to(repository_url(Repository.last))
-      end
     end
 
     context "with invalid parameters" do
@@ -74,41 +54,6 @@ RSpec.describe "/repositories", type: :request do
         expect {
           post repositories_url, params: { repository: invalid_attributes }
         }.to change(Repository, :count).by(0)
-      end
-
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post repositories_url, params: { repository: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested repository" do
-        repository = Repository.create! valid_attributes
-        patch repository_url(repository), params: { repository: new_attributes }
-        repository.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the repository" do
-        repository = Repository.create! valid_attributes
-        patch repository_url(repository), params: { repository: new_attributes }
-        repository.reload
-        expect(response).to redirect_to(repository_url(repository))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        repository = Repository.create! valid_attributes
-        patch repository_url(repository), params: { repository: invalid_attributes }
-        expect(response).to be_successful
       end
     end
   end
