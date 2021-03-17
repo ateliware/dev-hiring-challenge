90b8d0c7a41a5c55d16247f35a3a151a380a5146
Bianka Passos <biankatpas@gmail.com>
2021-03-17 00:49:00 -0300

    fix

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('githubapi.settings.heroku', 'githubapi.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'githubapi.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
