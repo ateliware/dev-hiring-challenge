aed455306e20bd169cda59080423d3bd2d3f6e29
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-17 02:52:12 -0300

    Add fallback

--- github-details/app/controllers/application_controller.rb
+++ github-details/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::UnknownFormat do |exception|
+    redirect_to(controller: request.params[:controller], action: request.params[:action], format: "html")
+  end
+
   def status
     @date = Date.current
   end
