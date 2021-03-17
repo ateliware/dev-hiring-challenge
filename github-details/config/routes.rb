dd1afa2c938297312f724b4eec380394144232b7
Gabriel Naoto <gabrielnaoto@yahoo.com.br>
2021-03-17 02:58:50 -0300

    Remove fallback

--- github-details/config/routes.rb
+++ github-details/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: "organizations#index"
 
-  defaults format: :html do
-    resources :repositories, only: [:index, :create, :destroy]
-    resources :organizations, param: :slug, only: [:index, :show]
-
-    get '/status', to: "application#status"
-  end
+  resources :repositories, only: [:index, :create, :destroy]
+  resources :organizations, param: :slug, only: [:index, :show]
 end
