c57bc457d1b48ef50b659071ea99699442086d72
Tiago Junior Pereira <tiago@cfaz.net>
2021-03-22 08:58:32 -0300

    Precompile assets

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,13 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.config.merge({
+    resolve: {
+        alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+})
+
 module.exports = environment
