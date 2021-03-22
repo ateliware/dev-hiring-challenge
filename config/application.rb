d954149fc8bfb2998b874b92d0dfa11d2c9b345d
Tiago Junior Pereira <tiago@cfaz.net>
2021-03-22 08:08:14 -0300

    Deploy production

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module DevHiringChallenge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
