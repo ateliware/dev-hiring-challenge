1873ba92f64bfc0958a5c44870772db6a456cff4
Bianka Passos <biankatpas@gmail.com>
2021-03-21 21:41:52 -0300

    refactor

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
-    path('repo_detail/<int:repo_id>/', views.repo_detail, name='repo_detail'),
+    path('load_repos', views.load_repos, name='load_repos'),
+    path('delete_repos', views.delete_repos, name='delete_repos'),
+    path('repo_detail/<int:repo_id>/', views.get_repo, name='repo_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,41 +6,52 @@ import requests
 from .models import Repo
 
 
+LANGUAGES = ('java', 'python', 'ruby', 'rust', 'go')
+
+
+def load_repos(request):
+    print('home:load_repos')
+    Repo.objects.all().delete()
+    try:
+        for language in LANGUAGES:
+            url = 'https://api.github.com/search/repositories?q=language:' + language + '&sort=stars&order=desc'
+            response = requests.get(url)
+            data = response.json()
+            repos = data['items']
+            for i in repos:
+                repo_data = Repo(
+                    name=i['name'],
+                    language=i['language'],
+                    stars=i['stargazers_count'],
+                    forks=i['forks_count'],
+                    description=i['description'],
+                    url=i['html_url'],
+                )
+                repo_data.save()
+    except KeyError:
+        raise Http404('API rate limit exceeded, please wait before updating again.')
+
+
+def delete_repos(request):
+    print('home:delete_repos')
+    Repo.objects.all().delete()
+
+
 def home(request):
+    print('home')
     if request.GET:
         if 'Atualizar' in request.GET.get('enviar'):
-            Repo.objects.all().delete()
-            try:
-                for language in ('java', 'python', 'ruby', 'rust', 'go'):
-                    url = 'https://api.github.com/search/repositories?q=language:'+language+'&sort=stars&order=desc'
-                    response = requests.get(url)
-                    data = response.json()
-                    repos = data['items']
-                    for i in repos:
-                        repo_data = Repo(
-                            name=i['name'],
-                            language=i['language'],
-                            stars=i['stargazers_count'],
-                            forks=i['forks_count'],
-                            description=i['description'],
-                            url=i['html_url'],
-                        )
-                        repo_data.save()
-            except KeyError:
-                raise Http404('API rate limit exceeded, please wait before updating again.')
-        elif 'Limpar' in request.GET.get('enviar'):
-            Repo.objects.all().delete()
+            load_repos(request)
+        elif request.GET and 'Limpar' in request.GET.get('enviar'):
+            delete_repos(request)
     all_repos = Repo.objects.all().order_by('-stars')
-    return render(request, 'home.html', {
-        "repos": all_repos,
-    })
+    return render(request, 'home.html', {"repos": all_repos})
 
 
-def repo_detail(request, repo_id):
+def get_repo(request, repo_id):
+    print('get_repo')
     try:
         repo = Repo.objects.get(id=repo_id)
     except Repo.DoesNotExist:
         raise Http404('repo not found')
-    return render(request, 'repo_detail.html', {
-        'repo': repo,
-    })
+    return render(request, 'repo_detail.html', {'repo': repo})
