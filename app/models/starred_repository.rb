04c1771a36f1bfdb2b2ff78650a1b1037b0521bf
Leonardo de Oliveira da Silva <los.oliveirasilva@gmail.com>
2021-03-28 20:16:33 -0300

    fix star repository when repo already exists

--- app/models/starred_repository.rb
+++ app/models/starred_repository.rb
@@ -3,4 +3,14 @@ class StarredRepository < ApplicationRecord
   belongs_to :repository
 
   accepts_nested_attributes_for :repository
+
+  before_validation :find_or_create_repository
+
+  private
+
+  def find_or_create_repository
+    repository = Repository.find_by(external_id: self.repository.external_id)
+
+    self.repository = repository if repository.present?
+  end
 end
