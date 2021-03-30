9601eef6d0da1cf92d78573405d0461530743908
Gabriel Barboza <gabrielmbarboza@gmail.com>
2021-03-29 23:35:28 -0300

    Cria seed para criacao das linguages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+%w[Ruby Python Javascript PHP Elixir].each {|language| Language.create(name: language)}
