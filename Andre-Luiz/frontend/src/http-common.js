0aaab70e81f62db983971073f2953ee67a63a50b
André Luiz Nunes Goulart <andreluizalemao@gmail.com>
2021-03-30 00:19:32 -0300

    Remove incorrect project

--- Andre-Luiz/frontend/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export default axios.create({
-  baseURL: "http://localhost:8080/api",
-  // baseURL: "https://ateliware-dev-test-backend.herokuapp.com/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-})
--- Andre-Luiz/frontend/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer: {
-    port: 8081
-  }
-}
