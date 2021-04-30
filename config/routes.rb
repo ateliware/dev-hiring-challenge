ad97a5b2b0f25611c594539ee884b88141b80888
Diego Haas Sanches <egoforia@gmail.com>
2021-04-30 20:42:23 -0300

    rota default

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/repos/search', to: 'repos#search', as: 'repos_search'
   resources :repos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'repos#index'
 end
