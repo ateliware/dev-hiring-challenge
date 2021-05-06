14020939bd0fb30709219748ad9b9f42124fd149
João <devel.neto@gmail.com>
2021-05-06 17:12:05 -0300

    Pequeno ajuste nos testes

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -16,10 +16,5 @@ RSpec.describe 'Homes', type: :request do
       expect(response).to redirect_to root_path
     end
 
-    it 'return http error' do
-      post '/search', params: {}
-
-      expect(response.alert).to ''
-    end
   end
 end
