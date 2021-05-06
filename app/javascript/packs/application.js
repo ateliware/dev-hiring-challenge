95f1db252e75f5dd22367dd6cab344a4f7f77701
João <devel.neto@gmail.com>
2021-05-06 16:40:32 -0300

    ajuste no js para carregar corretamente a fonte

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import 'bootstrap'
-import "@fortawesome/fontawesome-free/css/all"
+import "@fortawesome/fontawesome-free/js/all"
 import 'bootstrap-table'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
