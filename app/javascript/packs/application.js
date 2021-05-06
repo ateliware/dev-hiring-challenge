96c5bec51d57d280bb0299bcfd4d50a72b9d4a7d
João <devel.neto@gmail.com>
2021-05-06 14:06:02 -0300

    convertido de bulma para bootstrap adicionado alguns ajustes no css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
+import "@fortawesome/fontawesome-free/css/all"
+import 'bootstrap-table'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
