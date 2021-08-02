000c28927e3ed6ffddd744456a760f86122ca4e0
Carlos Grimm <grimmjunior@gmail.com>
2021-08-02 09:36:33 -0400

    feat: teste automatizado

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Devhiringchallenge
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #Rspec Configuration
+    config.generators do |g|
+      g.test_framework :rspec, :view_specs => false,
+                               :model_specs => false,
+                               :helper_specs => false,
+                               :routing_specs => false,
+                               :request_specs => false
+    end
   end
 end
