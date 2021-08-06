1a732f7a7c581e7b0b3fdf48b58e8dd632b25040
Camille Bettini <camille_bettini@icloud.com>
2021-08-05 21:56:41 -0300

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Elixir runtime as a parent image
+FROM elixir:latest
+
+RUN apt-get update && \
+  apt-get install -y postgresql-client
+
+# Create app directory and copy the Elixir projects into it
+RUN mkdir /app
+COPY . /app
+WORKDIR /app
+
+# Install hex package manager
+RUN mix local.hex --force
+
+# Compile the project
+RUN mix do compile
+
+CMD ["/app/entrypoint.sh"]
--- lib/github_project/repo.ex
+++ lib/github_project/repo.ex
@@ -2,4 +2,16 @@ defmodule GithubProject.Repo do
   use Ecto.Repo,
     otp_app: :github_project,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config =
+      config
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer())
+
+    {:ok, config}
+  end
 end
