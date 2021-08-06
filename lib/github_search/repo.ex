7954514fe8580de3ef4c0de47ac7eedfae5ade1a
Camille Bettini <camille_bettini@icloud.com>
2021-08-05 23:37:07 -0300

    Revert "dockerize"
    This reverts commit 74545a30aa976dc4e304bfcd2f7c722c6646a23c.

--- lib/github_search/repo.ex
+++ lib/github_search/repo.ex
@@ -2,16 +2,4 @@ defmodule GithubSearch.Repo do
   use Ecto.Repo,
     otp_app: :github_search,
     adapter: Ecto.Adapters.Postgres
-
-  def init(_, config) do
-    config =
-      config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer())
-
-    {:ok, config}
-  end
 end
