6f7351fb58ac74eea311e5435c99f3f661904136
Camille Bettini <camille_bettini@icloud.com>
2021-08-06 23:13:44 -0300

    add httpoison

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule GithubSearch.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
