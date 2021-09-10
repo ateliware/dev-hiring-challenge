72b2cb5554ad225f041e044dd0d3826718783df0
Ginetom Siqueira <ramires77@gmail.com>
2021-09-09 22:11:21 -0300

    Update docker files and setup script

--- src/setup.sh
+++ src/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 MIX_ENV=$ENV mix deps.get
+
+cd assets && npm install && node node_modules/webpack/bin/webpack.js --mode development
+cd ..
+
 MIX_ENV=$ENV mix compile
 
 MIX_ENV=$ENV mix ecto.create
