a6fddd5cde5d56571fe92762668106029401db31
William <178344@upf.br>
2022-01-14 14:45:01 -0300

    feat: add routes for SaveRepositoriesController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SaveRepositoriesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SaveRepositoriesController::class, 'index'])->name('repositories.index');
