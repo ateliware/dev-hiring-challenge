3530190abf250d56d76b737a82b40678c8197873
luiz.gomes@connect.com.br <luizgzanella@gmail.com>
2022-03-29 18:16:30 -0300

    fix reponsive

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
     const fetchRepository = async (language) => {
         try {
-            const response = await axios.get(`http://localhost:6868/most-starred/${language}`)
+            const response = await axios.get(`http://ec2-3-85-190-45.compute-1.amazonaws.com:6868/most-starred/${language}`)
             return response.data.items;
         } catch (error) {
             console.log(error)
--- front/src/components/section/section.js
+++ front/src/components/section/section.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './section.css';
 
 const SectionComponent = ({ children }) => {
   return <section className='main'>{children}</section>;
