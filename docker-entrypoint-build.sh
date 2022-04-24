36a76ed3c51013a1f8aa2ac6a8501736a3fbc025
Guilherme <guilherme.kaliter@gmail.com>
2022-04-24 12:31:40 -0300

    .

--- docker-entrypoint-build.sh
+++ docker-entrypoint-build.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
 
-
-# Get environment variables to show up in SSH session
-eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to get ENV variables: $status"
-  exit $status
-fi
-
 _build/$MIX_ENV/rel/dev_challenge/bin/dev_challenge start
 status=$?
 if [ $status -ne 0 ]; then
