73b8dcca09efa18b03d710fd0e3533ce442d86ed
Guilherme <guilherme.kaliter@gmail.com>
2022-04-24 19:18:57 -0300

    .

--- docker-entrypoint-build.sh
+++ docker-entrypoint-build.sh
@@ -4,12 +4,6 @@ while read p; do
   export ${p}
 done <.env
 
-_build/$MIX_ENV/rel/dev_challenge/bin/dev_challenge start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed get deps: $status"
-  exit $status
-fi
 mix phx.server
 status=$?
 if [ $status -ne 0 ]; then
