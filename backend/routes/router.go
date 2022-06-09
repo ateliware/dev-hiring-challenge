47a16f52d19cce00e483742a3a90323781ca090e
Eduardo Mior <eduardo-mior@hotmail.com>
2022-06-08 22:42:13 -0300

    feat: implementa middleware para tratar erros de 404 e 500 no BackEnd

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"desafio_ateliware/backend/controllers"
+	"desafio_ateliware/backend/models"
 	"desafio_ateliware/backend/repositories"
 
 	"github.com/gin-contrib/cors"
@@ -18,6 +19,21 @@ func SetupBackEndRoutes(router *gin.Engine) {
 	router.GET("/repositorios", controllers.BuscarRepositorios(githubRepository))
 	router.POST("/repositorios", controllers.SalvarRepositorios(githubRepository))
 	router.DELETE("/repositorios", controllers.ExcluirRepositorios(githubRepository))
+
+	// Tratando as requisições de EndPoints que não existem
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(400, models.Error{Error: "Recurso não encontrado"})
+	})
+
+	// Tratando as requisições que deram algum erro inesperado
+	router.Use(func(c *gin.Context) {
+		defer func() {
+			if r := recover(); r != nil {
+				c.JSON(500, models.Error{Error: "Ocorreu um erro interno ao tentar realizar a operação"})
+			}
+		}()
+		c.Next()
+	})
 
 	// Inicializando o servidor na porta 8080
 	go router.Run(":8080")
