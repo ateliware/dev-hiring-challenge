730fa4889a39894737b6e44b263257a4db3a7d87
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 02:21:27 -0300

    feat(repo.service): add findAll service

--- src/modules/repo/dto/update-repo.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateRepoInput } from './create-repo.input'
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql'
-
-@InputType()
-export class UpdateRepoInput extends PartialType(CreateRepoInput) {
-  @Field(() => Int)
-  id: number
-}
--- src/modules/repo/services/repo.service.ts
+++ src/modules/repo/services/repo.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { CreateRepoInput } from '../dto/create-repo.input'
-import { UpdateRepoInput } from '../dto/update-repo.input'
+import { GithubApiRepository } from '../repositories/github-api.repository'
+import { formatGithubRepositories } from '../utils/format-github-repositories.util'
 
 @Injectable()
 export class RepoService {
+  constructor(private githubApiRepository: GithubApiRepository) {}
+
   create(createRepoInput: CreateRepoInput) {
-    return 'This action adds a new repo'
+    return ''
   }
 
-  findAll() {
-    return `This action returns all repo`
-  }
+  async findAll() {
+    try {
+      const repositories = await Promise.all([
+        this.githubApiRepository.findAllRepos('javascript'),
+        this.githubApiRepository.findAllRepos('python'),
+        this.githubApiRepository.findAllRepos('c'),
+        this.githubApiRepository.findAllRepos('ruby'),
+        this.githubApiRepository.findAllRepos('elixir')
+      ])
 
-  findOne(id: number) {
-    return `This action returns a #${id} repo`
-  }
+      const formattedRepositories = repositories.map(repository =>
+        formatGithubRepositories(repository)
+      )
 
-  update(id: number, updateRepoInput: UpdateRepoInput) {
-    return `This action updates a #${id} repo`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} repo`
+      return formattedRepositories
+    } catch (error) {
+      throw new HttpException(
+        `Error to connect with GitHub API =(`,
+        HttpStatus.FAILED_DEPENDENCY
+      )
+    }
   }
 }
