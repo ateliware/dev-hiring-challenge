d3d6d9e1db5ed923aa2128083a88d3edb3b0941e
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 02:22:23 -0300

    feat(format-github-repositories.util): add util to format response properly

---
+++ src/modules/repo/utils/format-github-repositories.util.ts
@@ -0,0 +1,31 @@
+import { RepoFindAllOutput } from '../dto/outputs/repo-find-all.output'
+import { RepoFindAllResponseInput } from '../dto/responses/repo-find-all-response.input'
+
+function formatGithubRepositories(
+  apiResponse: RepoFindAllResponseInput
+): RepoFindAllOutput {
+  const repos = apiResponse.items.map(apiResponseItemData => ({
+    id: apiResponseItemData.id,
+    forks: apiResponseItemData.forks,
+    full_name: apiResponseItemData.full_name,
+    html_url: apiResponseItemData.html_url,
+    language: apiResponseItemData.language,
+    name: apiResponseItemData.name,
+    open_issues: apiResponseItemData.open_issues,
+    url: apiResponseItemData.url,
+    owner: apiResponseItemData.owner,
+    watchers: apiResponseItemData.watchers,
+    watchers_count: apiResponseItemData.watchers_count,
+    description: apiResponseItemData.description,
+    stargazers_count: apiResponseItemData.stargazers_count
+  }))
+
+  const language = repos[0].language
+
+  return {
+    language,
+    repositories: repos
+  }
+}
+
+export { formatGithubRepositories }
