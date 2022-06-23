68a2e7a22aa627c38551c3afbf8ef2a2ab7cc375
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 03:02:14 -0300

    feat(github-api.repository):  add findByName method

--- src/modules/repo/repositories/github-api.repository.ts
+++ src/modules/repo/repositories/github-api.repository.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@nestjs/common'
 import { GithubApiRoutes } from 'src/modules/shared/http/enums/github-api-routes.enum'
 import { githubApi } from 'src/modules/shared/http/github-api.http'
-import { RepoFindAllResponseInput } from '../dto/responses/repo-find-all-response.input'
+import { RepoFindResponseInput } from '../dto/responses/repo-find-response.input'
+import { Repo } from '../entities/repo.entity'
 
 @Injectable()
 class GithubApiRepository {
-  public async findAllRepos(
-    language: string
-  ): Promise<RepoFindAllResponseInput> {
-    const { data } = await githubApi.get(GithubApiRoutes.RepositorySearch, {
-      params: {
-        q: `language:${language}`,
-        sort: 'stars',
-        per_page: 5
+  public async findByName(repository_name: string): Promise<Repo> {
+    const { data } = await githubApi.get<RepoFindResponseInput>(
+      GithubApiRoutes.RepositorySearch,
+      {
+        params: {
+          q: `${repository_name} in:name`,
+          per_page: 1
+        }
       }
-    })
+    )
+
+    const repository = data?.items[0]
+
+    return repository
+  }
+  public async findAllRepos(language: string): Promise<RepoFindResponseInput> {
+    const { data } = await githubApi.get<RepoFindResponseInput>(
+      GithubApiRoutes.RepositorySearch,
+      {
+        params: {
+          q: `language:${language}`,
+          sort: 'stars',
+          per_page: 5
+        }
+      }
+    )
 
     return data
   }
