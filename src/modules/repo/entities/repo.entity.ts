b31b1067354c77ad88a7009dc58b7114bbed82c4
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 01:56:58 -0300

    feat(repo.entity): add repo entity on project:

--- src/modules/repo/entities/repo.entity.ts
+++ src/modules/repo/entities/repo.entity.ts
@@ -1,7 +1,44 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { Owner } from './owner.entity'
 
 @ObjectType()
 export class Repo {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field(() => Int)
+  id: number
+
+  @Field(() => String)
+  name: string
+
+  @Field(() => String)
+  full_name: string
+
+  @Field(() => String)
+  description?: string
+
+  @Field(() => String)
+  html_url: string
+
+  @Field(() => String)
+  url: string
+
+  @Field(() => Int)
+  stargazers_count?: number
+
+  @Field(() => Int)
+  watchers_count: number
+
+  @Field(() => String)
+  language: string
+
+  @Field(() => Int)
+  open_issues: number
+
+  @Field(() => Int)
+  forks: number
+
+  @Field(() => Int)
+  watchers: number
+
+  @Field(() => Owner)
+  owner: Owner
 }
