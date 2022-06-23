691b1375de875a0999edd0e235b1114afb005377
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-22 23:08:33 -0300

    chore(app): add env for application PORT

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const { APP_PORT } = process.env
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(APP_PORT)
+
+  console.log(`🎊  Server is up on ${APP_PORT}!  🚀\n`)
 }
+
 bootstrap()
