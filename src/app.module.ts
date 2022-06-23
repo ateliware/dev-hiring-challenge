827f68b34fb39da8bf1f407968889e81bf248914
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 02:01:29 -0300

    feat(app.module): add GraphQL configuration and repo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { RepoModule } from './modules/repo/repo.module'
 import { SharedModule } from './modules/shared/shared.module'
 
 @Module({
-  imports: [SharedModule]
+  imports: [
+    SharedModule,
+    RepoModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.NODE_ENV !== 'production',
+      autoSchemaFile: true
+    })
+  ]
 })
 export class AppModule {}
