e630711b2ac8d185f6ba19193de984a0c04855c4
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 01:56:16 -0300

    refactor(main): change server up message

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(APP_PORT)
 
-  console.log(`🎊  Server is up on ${APP_PORT}!  🚀\n`)
+  console.log(`🎊  Server is up on ${APP_PORT} 🚀\n`)
 }
 
 bootstrap()
