143b06adbf5d4ba3d46bc86318a5f368b1c142a0
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 02:23:57 -0300

    feat(repo.module): add GithubApiRepository provider

--- src/modules/repo/repo.module.ts
+++ src/modules/repo/repo.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
+import { GithubApiRepository } from './repositories/github-api.repository'
 import { RepoResolver } from './resolvers/repo.resolver'
 import { RepoService } from './services/repo.service'
 
 @Module({
-  providers: [RepoResolver, RepoService]
+  providers: [RepoResolver, RepoService, GithubApiRepository]
 })
 export class RepoModule {}
