6476bf6d41cc694b625b4055ccf751592d2c654c
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 01:59:34 -0300

    refactor(shared.module): removed unused providers

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
-import { SharedService } from './shared.service'
-import { SharedResolver } from './shared.resolver'
 
 @Module({
-  providers: [SharedResolver, SharedService]
+  providers: []
 })
 export class SharedModule {}
