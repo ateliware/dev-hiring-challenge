3955c16d0a6ec6c04c15cb850e7345f7fcbe62d1
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 22:31:08 -0300

    feat(owner.entity): add TypeORM entity and column decorators

--- src/modules/repo/entities/owner.entity.ts
+++ src/modules/repo/entities/owner.entity.ts
@@ -1,25 +1,58 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql'
+import { OWNER_TABLE_NAME } from 'src/modules/shared/constantes/database.constant'
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm'
 
 @ObjectType()
+@Entity(OWNER_TABLE_NAME)
 export class Owner {
   @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  db_id?: number
+
+  @Field(() => Int)
+  @Column()
   id: number
 
+  @Column()
   @Field(() => String)
-  login: string
+  login?: string
 
+  @Column()
   @Field(() => String)
-  avatar_url: string
+  avatar_url?: string
 
+  @Column()
   @Field(() => String)
-  url: string
+  url?: string
 
+  @Column()
   @Field(() => String)
-  html_url: string
+  html_url?: string
 
+  @Column()
   @Field(() => String)
-  repos_url: string
+  repos_url?: string
 
+  @Column()
   @Field(() => String)
-  type: 'Organization' | 'User'
+  type?: 'Organization' | 'User'
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  created_at?: Date
+
+  @UpdateDateColumn()
+  @Field(() => Date)
+  updated_at?: Date
+
+  @DeleteDateColumn()
+  @Field(() => Date)
+  deleted_at?: Date
 }
