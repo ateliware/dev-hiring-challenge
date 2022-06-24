6b9350a681557b85507d1a24f75b0fcefd6568d5
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-24 00:52:15 -0300

    tests(fake-repo.provider): add fake Repo Provider(Repository)

---
+++ src/modules/repo/repositories/fakes/fake-repo.provider.ts
@@ -0,0 +1,27 @@
+import { Repo } from '../../entities/repo.entity'
+
+class FakeRepoProvider {
+  private repositories: Repo[] = []
+
+  create(): Repo {
+    return new Repo()
+  }
+
+  async find(): Promise<Repo[]> {
+    return this.repositories
+  }
+
+  async findOne(name: string): Promise<Repo> {
+    const repository = this.repositories.find(
+      repository => repository.name === name
+    )
+    return repository
+  }
+
+  async save(entity: Repo): Promise<any> {
+    this.repositories.push(entity as Repo)
+    return entity
+  }
+}
+
+export { FakeRepoProvider }
