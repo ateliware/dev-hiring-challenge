252913f6e509ae1e5b126f008e283356473a7b8f
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 23:50:12 -0300

    feat(repo.service): add create repo Service

--- src/modules/repo/services/repo.service.ts
+++ src/modules/repo/services/repo.service.ts
@@ -1,14 +1,44 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
 import { CreateRepoInput } from '../dto/create-repo.input'
+import { Repo } from '../entities/repo.entity'
 import { GithubApiRepository } from '../repositories/github-api.repository'
+import { REPO_REPOSITORY_PROVIDER } from '../repositories/repo.provider'
 import { formatGithubRepositories } from '../utils/format-github-repositories.util'
 
 @Injectable()
 export class RepoService {
-  constructor(private githubApiRepository: GithubApiRepository) {}
+  constructor(
+    @Inject(REPO_REPOSITORY_PROVIDER)
+    private repoRepository: Repository<Repo>,
+    private githubApiRepository: GithubApiRepository
+  ) {}
 
-  create(createRepoInput: CreateRepoInput) {
-    return ''
+  async create(createRepoInput: CreateRepoInput) {
+    const repoAlreadyExists = await this.repoRepository.findOne({
+      where: {
+        id: createRepoInput.id
+      }
+    })
+
+    if (!repoAlreadyExists) {
+      const repo = this.repoRepository.create()
+
+      Object.assign(repo, createRepoInput)
+
+      const createdRepo = await this.repoRepository.save(repo)
+      repo.is_storaged = true
+
+      return createdRepo
+    } else
+      throw new HttpException(
+        `This repo ${
+          repoAlreadyExists?.full_name ||
+          repoAlreadyExists.name ||
+          repoAlreadyExists.id
+        } is already saved, try another repo!`,
+        HttpStatus.CONFLICT
+      )
   }
 
   async findOne(repository_full_name: string) {
