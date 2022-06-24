9c1c7562acd62ffa7550040eea70b14773803452
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 22:33:55 -0300

    feat(repo.provider): add repo repository provider

---
+++ src/modules/repo/repositories/repo.provider.ts
@@ -0,0 +1,13 @@
+import { DATASOURCE_PROVIDER } from 'src/modules/shared/constantes/database.constant'
+import { DataSource } from 'typeorm'
+import { Repo } from '../entities/repo.entity'
+
+export const REPO_REPOSITORY_PROVIDER = 'REPO_REPOSITORY'
+
+export const RepoRepository = [
+  {
+    provide: REPO_REPOSITORY_PROVIDER,
+    useFactory: (dataSource: DataSource) => dataSource.getRepository(Repo),
+    inject: [DATASOURCE_PROVIDER]
+  }
+]
