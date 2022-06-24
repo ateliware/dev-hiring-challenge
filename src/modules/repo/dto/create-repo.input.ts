f7373c0001c630eeebe9165ba54f91d96fbf05d7
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-23 23:47:51 -0300

    feat(create-repo.input): add input fields

--- src/modules/repo/dto/create-repo.input.ts
+++ src/modules/repo/dto/create-repo.input.ts
@@ -2,6 +2,39 @@ import { InputType, Int, Field } from '@nestjs/graphql'
 
 @InputType()
 export class CreateRepoInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field(() => Int)
+  id: number
+
+  @Field(() => String, { nullable: true })
+  name?: string
+
+  @Field(() => String, { nullable: true })
+  full_name?: string
+
+  @Field(() => String, { nullable: true })
+  description?: string
+
+  @Field(() => String, { nullable: true })
+  html_url?: string
+
+  @Field(() => String, { nullable: true })
+  url?: string
+
+  @Field(() => Int, { nullable: true })
+  stargazers_count?: number
+
+  @Field(() => Int, { nullable: true })
+  watchers_count?: number
+
+  @Field(() => String, { nullable: true })
+  language?: string
+
+  @Field(() => Int, { nullable: true })
+  open_issues?: number
+
+  @Field(() => Int, { nullable: true })
+  forks?: number
+
+  @Field(() => Int, { nullable: true })
+  watchers?: number
 }
