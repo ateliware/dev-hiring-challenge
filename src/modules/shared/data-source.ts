909865e7004ffe8cf5283bce2b82dccfa583787d
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-25 20:20:19 -0300

    chore(.env.sample): change DB_URL to DATABASE_URL

--- src/modules/shared/data-source.ts
+++ src/modules/shared/data-source.ts
@@ -2,13 +2,13 @@ import 'dotenv/config'
 
 import { DataSource } from 'typeorm'
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_URL } = process.env
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DATABASE_URL } = process.env
 
 const migrationPath = `${__dirname}/migrations/*.{ts,js}`
 const entitiesPath = `${__dirname}/../**/entities/*.entity{.ts,.js}`
 
-if (DB_URL && (DB_HOST || DB_PORT || DB_USERNAME || DB_PASSWORD || DB_DATABASE))
-  throw new Error('DB_URL is allowed only on production server.')
+if (DATABASE_URL && (DB_HOST || DB_PORT || DB_USERNAME || DB_PASSWORD || DB_DATABASE))
+  throw new Error('DATABASE_URL is allowed only on production server.')
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -16,11 +16,11 @@ export const AppDataSource = new DataSource({
   port: Number(DB_PORT),
   username: DB_USERNAME,
   password: DB_PASSWORD,
-  url: DB_URL,
+  url: DATABASE_URL,
   database: DB_DATABASE,
   entities: [entitiesPath],
   migrations: [migrationPath],
-  ssl: DB_URL
+  ssl: DATABASE_URL
     ? {
         rejectUnauthorized: false
       }
