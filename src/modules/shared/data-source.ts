251b23d60b645f51ff128e654095f3c19f76a2de
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-25 20:10:52 -0300

    refactor(data-source): add DB_URL on SSL option

--- src/modules/shared/data-source.ts
+++ src/modules/shared/data-source.ts
@@ -2,10 +2,13 @@ import 'dotenv/config'
 
 import { DataSource } from 'typeorm'
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_URL } = process.env
 
 const migrationPath = `${__dirname}/migrations/*.{ts,js}`
 const entitiesPath = `${__dirname}/../**/entities/*.entity{.ts,.js}`
+
+if (DB_URL && (DB_HOST || DB_PORT || DB_USERNAME || DB_PASSWORD || DB_DATABASE))
+  throw new Error('DB_URL is allowed only on production server.')
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -13,10 +16,9 @@ export const AppDataSource = new DataSource({
   port: Number(DB_PORT),
   username: DB_USERNAME,
   password: DB_PASSWORD,
+  url: DB_URL,
   database: DB_DATABASE,
   entities: [entitiesPath],
   migrations: [migrationPath],
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: DB_URL ? true : false
 })
