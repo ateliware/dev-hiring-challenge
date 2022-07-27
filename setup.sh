e153d3132705d41267721ef32d8515a3aef81c4c
LucasBergaminAX4B <lucas.santos@ax4b.com>
2022-07-26 22:02:08 -0300

    add config HEROKU

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 mkdir -p ~/.streamlit/
 echo "\
-[theme]
-primaryColor='#7f91ef'
-backgroundColor='#92a1f9'
-secondaryBackgroundColor='#7f91ef'
-textColor='#fbfbfb'
-"
-echo "\
 [general]\n\
 email = \"lbergaminsantos2020@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
@@ -15,4 +8,4 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+" >> ~/.streamlit/config.toml
