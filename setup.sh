133b79452398c87e08f5bbb9503ac2bd5494f804
LucasBergaminAX4B <lucas.santos@ax4b.com>
2022-07-26 21:54:17 -0300

    add config HEROKU

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]
+primaryColor='#7f91ef'
+backgroundColor='#92a1f9'
+secondaryBackgroundColor='#7f91ef'
+textColor='#fbfbfb'
+"
+echo "\
 [general]\n\
 email = \"lbergaminsantos2020@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
