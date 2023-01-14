7c59638264aac0af2908be8db7a6b5c22b2aee4a
bielzin <oliva.gabriel@outlook.com>
2023-01-14 14:02:12 -0300

    set app root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root "languages#index"
   resources :repositories
   resources :languages, param: :slug
   get 'results', to: 'results#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
