288c921f994d61dc69cd47af702006726f9bb124
bielzin <oliva.gabriel@outlook.com>
2023-01-14 23:52:28 -0300

    adds tests to githubmodelapi

--- spec/models/github_api_model_spec.rb
+++ spec/models/github_api_model_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe GithubApiModel, type: :model do
   # this is a flaky test, 'cause it depends of an external api call
-  describe 'something' do
+  context "when api is available" do
     let(:github_response) { GithubApiModel.retrieve_language_repo('javascript') }
-    it 'should do something' do
+    it "should return a hash with data from an repository" do
       expect(github_response).to be_kind_of(Hash)
       expect(github_response).to have_key("id")
       expect(github_response).to have_key("name")
