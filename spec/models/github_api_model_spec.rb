7fc929ebb05edb4a7e4d8c804a424cdd060d37f4
bielzin <oliva.gabriel@outlook.com>
2023-01-14 22:18:06 -0300

    adds test do GithubApiModel

--- spec/models/github_api_model_spec.rb
+++ spec/models/github_api_model_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GithubApiModel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # this is a flaky test, 'cause it depends of an external api call
+  describe 'something' do
+    let(:github_response) { GithubApiModel.retrieve_language_repo('javascript') }
+    it 'should do something' do
+      expect(github_response).to be_kind_of(Hash)
+      expect(github_response).to have_key("id")
+      expect(github_response).to have_key("name")
+      expect(github_response).to have_key("full_name")
+    end
+  end
 end
